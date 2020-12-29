c60ae8d88f03d27b919d8cd6bc8637475aaef3eb
“acaru” <alberto.canoruiz@gmail.com>
2020-12-29 21:47:21 +0100

    Version final PIXEL API 29

--- app/src/main/java/com/example/felicitacion/SplashActivity.java
+++ app/src/main/java/com/example/felicitacion/SplashActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -25,6 +26,9 @@ public class SplashActivity extends AppCompatActivity implements Animation.Anima
         texto.startAnimation(animacion);
 
         animacion.setAnimationListener(this);
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(this,R.raw.efectosplash);
+        mediaPlayer.start();
     }
 
     @Override
